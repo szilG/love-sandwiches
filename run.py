04f27da22f7294d9d805c95fbcc852491e143504
szilG <gombiszilvi@gmail.com>
2021-05-24 10:12:40 +0000

    Retrive latest stock mumbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,7 +21,7 @@ def get_sales_data():
     while True:
         print("Please enter sales data from the last market")
         print("Data sould be six numbers, separated by commas.")
-        print("Exemple: 10,20,30,40,50\n")
+        print("Exemple: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here:")
         
@@ -56,8 +57,26 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet is updated successfully.\n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
 
+    The surplus is defined as the sales figure subtrackted from the stock:
+    - Positive surplis indicate waste
+    - negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surpus data...\n")
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+main()
